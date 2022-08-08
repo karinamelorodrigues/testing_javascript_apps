18728556f40c09ed9baea91d13ac5379b16a53dd
Karina Melo Rodrigues <karina.rodrigues@willbank.com.br>
2022-08-07 22:33:53 -0300

    feat: adding Cart, queryString and calculator features

---
+++ src/lib/queryString.js
@@ -0,0 +1,17 @@
+const keyValueToString = ([key, value]) => {
+    if(typeof value === 'object' && !Array.isArray(value)) {
+        throw new Error('Please check your params')
+    }
+    return `${key}=${value}`
+}
+
+export const queryString = obj => 
+    Object.entries(obj).map(keyValueToString).join('&')
+
+export const parse = string => Object.fromEntries(string.split('&').map(item => { 
+    let [key, value] = item.split('=')
+    if(value.indexOf(',') > -1) {
+        value = value.split(',')
+    }
+    return [key, value]
+}))
